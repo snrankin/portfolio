9571f03e22c65d098dcf491bcd965b13a686df3a
Sam Rankin <samrankin.dev@gmail.com>
2023-04-02 21:35:47 -0700

    🎨 style(gatsby-browser.js): reformat code and add semicolons ✨ feat(gatsby-browser.js): add semicolons to the end of each statement 🐛 fix(gatsby-browser.js): fix shouldUpdateScroll function to use single quotes instead of double quotes 🐛 fix(gatsby-browser.js): fix onServiceWorkerUpdateReady function to use single quotes instead of double quotes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,29 +4,23 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
 
-
-export const shouldUpdateScroll = ({
-	routerProps: { location },
-	getSavedScrollPosition,
-}) => {
-	if (location.action === "PUSH") {
-		window.setTimeout(() => window.scrollTo(0, 0), 200)
+export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
+	if (location.action === 'PUSH') {
+		window.setTimeout(() => window.scrollTo(0, 0), 200);
 	} else {
-		const savedPosition = getSavedScrollPosition(location)
-		window.setTimeout(
-			() => window.scrollTo(...(savedPosition || [0, 0])),
-			200
-		)
+		const savedPosition = getSavedScrollPosition(location);
+		window.setTimeout(() => window.scrollTo(...(savedPosition || [0, 0])), 200);
 	}
-	return false
-}
+	return false;
+};
 
 // Display a message when a service worker updates
 // https://www.gatsbyjs.org/docs/add-offline-support-with-a-service-worker/#displaying-a-message-when-a-service-worker-updates
 export const onServiceWorkerUpdateReady = () => {
-	const div = document.createElement("div")
-	div.id = "toast"
-	div.classList.add("alert", "alert-info")
-	div.innerHTML = `<button onClick="window.location.reload()">Updates are available. <span>Click to Reload</span>.</button>`
-	document.body.append(div)
-}
+	const div = document.createElement('div');
+	div.id = 'toast';
+	div.classList.add('alert', 'alert-info');
+	div.innerHTML =
+		'<button onClick="window.location.reload()">Updates are available. <span>Click to Reload</span>.</button>';
+	document.body.append(div);
+};
