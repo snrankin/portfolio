ab4963ab8c248f8e8a6af0802b8a9e8ff56ef31c
Sam Rankin <samrankin.dev@gmail.com>
2023-04-02 21:34:33 -0700

    🎉 feat(eslint): add eslint configuration file 🎉 feat(eslintignore): add eslint ignore file

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+	root: true,
+	extends: [
+		'eslint:recommended', // use recommended configs
+		'plugin:react/recommended',
+		'plugin:gatsby/recommended',
+		'prettier',
+	],
+	plugins: ['react', 'gatsby'],
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		requireConfigFile: false,
+		allowImportExportEverywhere: true,
+		babelOptions: {
+			presets: ['@babel/preset-react'],
+		},
+	},
+	globals: {
+		window: true,
+		document: true,
+	},
+	rules: {
+		'no-unused-vars': 1,
+		'no-useless-escape': 0,
+		quotes: ['warn', 'single'],
+		'comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'ignore',
+			},
+		],
+		'react/prop-types': 1,
+	},
+	settings: {
+		react: {
+			version: 'detect', // detect react version
+		},
+	},
+	env: {
+		node: true, // defines things like process.env when generating through node
+	},
+};
