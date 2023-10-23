4c4b5d1feadd096ef0de5888b7d589b337fa5b47
Sam Rankin <srankin@riester.com>
2023-10-23 13:14:10 -0700

    🔧 chore(tailwind.config.ts): update tailwind configuration to include additional paths for content and add new plugins
    🔧 chore(tailwind.config.ts): update tailwind configuration to include additional paths for content and add new plugins. Also, configure the theme to include custom font families and extend the background image options.
    
    🔧 chore(tailwind.config.ts): update tailwind configuration to include additional paths for content and add new plugins. Also, configure the theme to include custom font families, extend the background image options, and enable dark mode.
    
    🔧 chore(tailwind.config.ts): update tailwind configuration to include additional paths for content and add new plugins. Also, configure the theme to include custom font families, extend the background image options, enable dark mode, and add daisyui plugin with custom settings.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/tw-elements-react/dist/js/**/*.js'],
+
+	theme: {
+		fontFamily: {
+			sans: ['Poppins'],
+			mono: ['Inconsolata'],
+		},
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	darkMode: 'class',
+	plugins: [
+		require('tw-elements/dist/plugin.cjs'),
+		require('@tailwindcss/typography')({
+			// all these options default to the values specified here
+			ellipsis: true, // whether to generate ellipsis utilities
+			hyphens: true, // whether to generate hyphenation utilities
+			kerning: true, // whether to generate kerning utilities
+			textUnset: true, // whether to generate utilities to unset text properties
+			componentPrefix: '', // the prefix to use for text style classes
+		}),
+		require('daisyui'),
+	],
+	daisyui: {
+		styled: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: '',
+		darkTheme: 'dark',
+		themes: false,
+	},
+};
+export default config;
