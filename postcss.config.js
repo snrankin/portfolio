9da5aeb2328439d27f24353c6ab64884696377b7
Sam Rankin <srankin@riester.com>
2023-10-23 13:37:24 -0700

    🔥 chore(.eslintrc.js): remove ESLint configuration file
    🔧 chore(.vscode/settings.json): add Visual Studio Code settings file
    
    📦 chore(package.json): update dependencies
    
    🔧 chore(postcss.config.js): update PostCSS configuration file to use new plugins and presets

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,36 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import litePreset from 'cssnano-preset-lite';
+import autoprefixer from 'autoprefixer';
+import postcssPresetEnv from 'postcss-preset-env';
+
+module.exports = (api) => {
+	const preset = process.env.NODE_ENV === 'production' ? litePreset({ discardComments: false }) : litePreset({ discardComments: false });
+	/** @type {import('postcss-load-config').Config} */
+	const config = {
+		plugins: [
+			'tailwindcss',
+			'postcss-viewport-height-correction',
+			'postcss-fixes',
+			'postcss-momentum-scrolling',
+			'postcss-easings',
+			'postcss-easing-gradients',
+			'postcss-combine-duplicated-selectors',
+			[
+				'postcss-sort-media-queries',
+				{
+					configuration: {
+						unitlessMqAlwaysFirst: true,
+					},
+				},
+			],
+			postcssPresetEnv({
+				/* pluginOptions */
+				features: {
+					'nesting-rules': {
+						noIsPseudoSelector: false,
+					},
+				},
+			}),
+		],
+	};
+	return config;
+};
