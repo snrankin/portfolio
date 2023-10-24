9ef93508b82c2d5d964f7be48077ac6872ae2280
Sam Rankin <srankin@riester.com>
2023-10-24 12:32:12 -0700

    🔧 chore(postcss.config.js): refactor postcss configuration to use require syntax for importing dependencies 🔧 chore(postcss.config.js): remove unnecessary code and simplify postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,22 @@
-import litePreset from 'cssnano-preset-lite';
-import autoprefixer from 'autoprefixer';
-import postcssPresetEnv from 'postcss-preset-env';
+const postcssPresetEnv = require('postcss-preset-env');
 
-module.exports = (api) => {
-	const preset = process.env.NODE_ENV === 'production' ? litePreset({ discardComments: false }) : litePreset({ discardComments: false });
-	/** @type {import('postcss-load-config').Config} */
-	const config = {
-		plugins: [
-			'tailwindcss',
-			'postcss-viewport-height-correction',
-			'postcss-fixes',
-			'postcss-momentum-scrolling',
-			'postcss-easings',
-			'postcss-easing-gradients',
-			'postcss-combine-duplicated-selectors',
-			[
-				'postcss-sort-media-queries',
-				{
-					configuration: {
-						unitlessMqAlwaysFirst: true,
-					},
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+	plugins: [
+		'tailwindcss',
+		'postcss-viewport-height-correction',
+		'postcss-fixes',
+		'postcss-momentum-scrolling',
+		'postcss-easings',
+		'postcss-easing-gradients',
+		'postcss-combine-duplicated-selectors',
+		[
+			'postcss-sort-media-queries',
+			{
+				configuration: {
+					unitlessMqAlwaysFirst: true,
 				},
-			],
-			postcssPresetEnv({
-				/* pluginOptions */
-				features: {
-					'nesting-rules': {
-						noIsPseudoSelector: false,
-					},
-				},
-			}),
+			},
 		],
-	};
-	return config;
+	],
 };
