59b79e50f8b8615b577aa83cd01faa69388b96f7
Sam Rankin <srankin@riester.com>
2023-10-24 12:32:28 -0700

    🔧 chore(tailwind.config.ts): remove unused code and clean up formatting for better readability ✨ feat(tailwind.config.ts): add custom font families and font sizes to extend the default theme ✨ feat(tailwind.config.ts): customize container settings for centering and padding ✨ feat(tailwind.config.ts): customize row settings for flex display and width 🔧 chore(tailwind.config.ts): remove commented out code and clean up formatting for better readability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,50 @@
 import type { Config } from 'tailwindcss';
-
+const defaultTheme = require('tailwindcss/defaultTheme');
 const config: Config = {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/tw-elements-react/dist/js/**/*.js'],
 
 	theme: {
-		fontFamily: {
-			sans: ['Poppins'],
-			mono: ['Inconsolata'],
-		},
 		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+
+				mono: ['Inconsolata', ...defaultTheme.fontFamily.mono],
 			},
+			fontSize: {
+				icon: [
+					'0.8em',
+					{
+						lineHeight: '1',
+					},
+				],
+			},
+		},
+
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '1.2rem',
+				lg: '1.5rem',
+				xl: '1.5rem',
+				'2xl': '1.5rem',
+			},
+			// default breakpoints but with 40px removed
+			screens: {
+				sm: '90vw',
+				md: '90vw',
+				lg: '85vw',
+				xl: '1170px',
+				'2xl': '1170px',
+			},
+		},
+		row: {
+			display: 'flex',
+			width: '100%',
 		},
 	},
 	darkMode: 'class',
-	plugins: [
-		require('tw-elements/dist/plugin.cjs'),
-		require('@tailwindcss/typography')({
-			// all these options default to the values specified here
-			ellipsis: true, // whether to generate ellipsis utilities
-			hyphens: true, // whether to generate hyphenation utilities
-			kerning: true, // whether to generate kerning utilities
-			textUnset: true, // whether to generate utilities to unset text properties
-			componentPrefix: '', // the prefix to use for text style classes
-		}),
-		require('daisyui'),
-	],
+	plugins: [require('tw-elements/dist/plugin.cjs'), require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
 		styled: true,
 		base: true,
@@ -36,7 +53,37 @@ const config: Config = {
 		rtl: false,
 		prefix: '',
 		darkTheme: 'dark',
-		themes: false,
+		// themes: [
+
+		// 	{
+		// 		light: {
+		// 			...require('daisyui/src/theming/themes')['[data-theme=light]'],
+		// 			primary: '#0D9488',
+		// 			secondary: '#DB2777',
+		// 			accent: '#7C3AED',
+		// 			neutral: '#18181B',
+		// 			'base-100': '#FAFAFA',
+		// 			info: '#0891B2',
+		// 			success: '#65A30D',
+		// 			warning: '#F59E0B',
+		// 			error: '#E11D48',
+		// 		},
+		// 	},
+		// 	{
+		// 		dark: {
+		// 			...require('daisyui/src/theming/themes')['[data-theme=dark]'],
+		// 			primary: '#2DD4BF',
+		// 			secondary: '#F472B6',
+		// 			accent: '#A855F7',
+		// 			neutral: '#FAFAFA',
+		// 			'base-100': '#18181B',
+		// 			info: '#67E8F9',
+		// 			success: '#BEF264',
+		// 			warning: '#FCD34D',
+		// 			error: '#FDA4AF',
+		// 		},
+		// 	},
+		// ],
 	},
 };
 export default config;
