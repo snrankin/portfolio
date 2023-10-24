e137f495065bf5e5ee465b2a836ff7b3ae0a23e9
Sam Rankin <srankin@riester.com>
2023-10-24 12:31:25 -0700

    🔧 chore(next.config.js): add configuration for images and webpack in next.config.js
    📸 images:
    - Added support for remote patterns in the images configuration.
    - Added a remote pattern for the faviconV2 image hosted on t1.gstatic.com.
    
    ⚙️ webpack:
    - Added a new rule to handle SVG files using the `@svgr/webpack` loader.
    - The rule is applied to SVG files that are imported in JavaScript or TypeScript files.
    
    No functional changes were made, only configuration updates.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 't1.gstatic.com',
+				port: '',
+				pathname: '/faviconV2',
+			},
+		],
+	},
+	webpack: (config) =>  {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
+		});
+		return config;
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
