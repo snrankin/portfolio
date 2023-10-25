b1a09f2f2a0ad2750d66fef953aceb7fce4035d5
Sam Rankin <srankin@riester.com>
2023-10-24 21:24:21 -0700

    🎨 style(header.tsx): update import paths for Logo, Close, and Menu components to match new file locations 🔧 chore(header.tsx): add Waypoint component to handle scroll events and update header background and drop shadow based on scroll position 🔧 chore(header.tsx): add handleExit and handleEnter functions to update isScrolled state based on scroll position

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,9 +3,11 @@
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import Logo from '@/app/logo.svg';
+import Logo from '@/icons/logo.svg';
+import Close from '@/icons/close.svg';
+import Menu from '@/icons/menu.svg';
 import classNames from 'classnames';
-
+import { Waypoint } from 'react-waypoint';
 const ThemeButton = dynamic(() => import('./themeButton'), {
 	ssr: false,
 });
@@ -15,6 +17,7 @@ const Nav = dynamic(() => import('./nav'), {
 });
 
 export default function Header(): JSX.Element {
+	let [isScrolled, setScrolled] = useState(false);
 	let headerClasses = classNames({
 		fixed: true,
 		'top-0': true,
@@ -22,36 +25,57 @@ export default function Header(): JSX.Element {
 		'w-screen': true,
 		'transition-all': true,
 		'duration-200': true,
-		'bg-base-100': true,
+		'bg-transparent': !isScrolled,
+		'bg-base-100': isScrolled,
 		'z-50': true,
+		'drop-shadow-md': isScrolled,
+		'drop-shadow-none': !isScrolled,
 	});
+
+	const handleExit = (e: any) => {
+		if (e.currentPosition == 'above') {
+			setScrolled(true);
+		}
+	};
+
+	const handleEnter = (e: any) => {
+		if (e.currentPosition == 'inside') {
+			setScrolled(false);
+		}
+	};
+
 	return (
-		<header className={headerClasses}>
-			<nav className="navbar">
-				<div className="navbar-start">
-					<div className="dropdown">
-						<label className="btn btn-circle swap swap-rotate lg:hidden">
-							<input type="checkbox" />
-							<svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
-								<path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-							</svg>
-							<svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
-								<polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-							</svg>
-						</label>
-						<Nav mobile={true} />
+		<>
+			<header id="site-header" className={headerClasses}>
+				<nav className="navbar">
+					<div className="navbar-start">
+						<Link href="/" className="w-[40px] lg:w-[60px] block">
+							<Logo className="fill-base-content w-full" />
+						</Link>
 					</div>
-					<Link href="/" className="w-[40px] lg:w-[60px] block">
-						<Logo className="fill-base-content w-full" />
-					</Link>
-				</div>
-				<div className="navbar-center hidden lg:flex">
-					<Nav />
-				</div>
-				<div className="navbar-end">
-					<ThemeButton />
-				</div>
-			</nav>
-		</header>
+					<div className="navbar-center hidden lg:flex">
+						<Nav />
+					</div>
+					<div className="navbar-end">
+						<ThemeButton />
+						<div className="dropdown">
+							<label className="btn  btn-circle aspect-square w-11 btn-primary swap swap-rotate lg:hidden">
+								<input type="checkbox" />
+								<span className="icon swap-on">
+									<Menu className="block stroke-2" />
+								</span>
+								<span className="icon swap-on">
+									<Close className="block stroke-2" />
+								</span>
+							</label>
+							<Nav mobile={true} />
+						</div>
+					</div>
+				</nav>
+			</header>
+			<Waypoint onEnter={handleEnter} onLeave={handleExit}>
+				<div className="spacer min-h-[80px] -mb-[80px] z-50 static"></div>
+			</Waypoint>
+		</>
 	);
 }
