978a6e9c63ba0895a6524c5f88bd47386d813a85
Sam Rankin <srankin@riester.com>
2023-10-24 21:26:39 -0700

    🔨 refactor(work-history.tsx): refactor work history section to use a reusable Section component for improved code organization and readability

--- src/app/components/work-history.tsx
+++ src/app/components/work-history.tsx
@@ -3,21 +3,20 @@ import React from 'react';
 import { default as resume } from '@/app/data/resume.json';
 import TimelineItem from './timeline-item';
 import Heading from './heading';
+
+import Section from './section';
 export default function Timeline(): JSX.Element {
 	return (
-		<section id="work-history">
-			<div className="container mx-auto">
-				<Heading title="Work History" displayTitle="work" flag="history" />
-				<ol className="border-l-2 border-info-100">
-					{resume.work.map((job, i) => {
-						return (
-							<li key={i}>
-								<TimelineItem {...job} />
-							</li>
-						);
-					})}
-				</ol>
-			</div>
-		</section>
+		<Section id="work-history" title="Work" displayTitle="work" flag="history">
+			<ol className="border-l-2 border-info-100 grid grid-cols-1 gap-10">
+				{resume.work.map((job, i) => {
+					return (
+						<li key={i}>
+							<TimelineItem {...job} />
+						</li>
+					);
+				})}
+			</ol>
+		</Section>
 	);
 }
