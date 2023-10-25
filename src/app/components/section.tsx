dc25ca8a1510ef9d1ab075ce5bac257be84d4415
Sam Rankin <srankin@riester.com>
2023-10-24 21:25:35 -0700

    🔧 refactor(section.tsx): remove unused handleEnter function to improve code readability and maintainability 🔧 refactor(section.tsx): conditionally render Heading component based on showTitle prop to provide flexibility in displaying section titles

--- src/app/components/section.tsx
+++ src/app/components/section.tsx
@@ -3,17 +3,14 @@ import React, { useState } from 'react';
 import { Waypoint } from 'react-waypoint';
 import Heading from './heading';
 export default function Section(props: any) {
-	const handleEnter = (e: any) => {
-		console.log('🚀 ~ file: section.tsx:10 ~ handleEnter ~ e:', e);
-	};
-
 	let children: React.ReactNode = props.children;
 
 	return (
-		<Waypoint onEnter={handleEnter}>
+		<Waypoint>
 			<section id={props.id} className="lg:py-32 min-h-screen">
 				<div className="container mx-auto">
-					<Heading title={props.title} displayTitle={props.displayTitle} flag={props.flag} />
+					{props.showTitle ? <Heading title={props.title} displayTitle={props.displayTitle} flag={props.flag} /> : null}
+
 					{children}
 				</div>
 			</section>
