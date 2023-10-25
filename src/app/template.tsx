425d283bf75b12c5e2c5eeab0faabe82007a9919
Sam Rankin <srankin@riester.com>
2023-10-24 21:28:09 -0700

    🎨 style(page.tsx): add Projects component to improve page layout and content 🎨 style(template.tsx): add w-full and overflow-y-none classes to main element to improve page layout and prevent vertical scrolling

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -10,7 +10,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<Header />
-			<main className="flex min-h-screen flex-col">{children}</main>
+			<main className="flex min-h-screen flex-col w-full overflow-y-none">{children}</main>
 			<Footer />
 		</>
 	);
