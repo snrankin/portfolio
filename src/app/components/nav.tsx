a64b3b960ce9a79c12e334c3a1e4daf0594a3a5d
Sam Rankin <srankin@riester.com>
2023-10-24 21:24:41 -0700

    🔧 fix(nav.tsx): update z-index value to 50 and add site-nav class to improve styling and positioning of the navigation component 🔧 fix(nav.tsx): update data-section attribute values to improve accessibility and navigation within the page

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -8,17 +8,18 @@ export default function Nav(props: any): JSX.Element {
 		'menu-sm': props.mobile,
 		'menu-horizontal': !props.mobile,
 		'dropdown-content': props.mobile,
-		'z-[1]': true,
+		'z-[50]': true,
+		'site-nav': true,
 	});
 	return (
 		<ul className={menuClasses}>
-			<li>
+			<li data-section="about">
 				<Link href="#about">About</Link>
 			</li>
-			<li>
+			<li data-section="projects">
 				<Link href="#projects">Projects</Link>
 			</li>
-			<li>
+			<li data-section="work-history">
 				<Link href="#work-history">Work History</Link>
 			</li>
 		</ul>
