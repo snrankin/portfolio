ca58690b6f2d7d2c0ba3b0f749df16d57e007984
Sam Rankin <samrankin.dev@gmail.com>
2023-10-28 20:10:08 -0700

    🎨 style(tabs.tsx): update import paths for icons and images, and reformat code for better readability 🔧 chore(tabs.tsx): refactor code to use Device component for displaying different device views The import paths for icons and images have been updated to reflect the correct file locations. The code has been reformatted to improve readability and maintainability. The code has also been refactored to use the Device component for displaying different device views, making it easier to manage and customize the device-specific content.

--- src/app/components/tabs.tsx
+++ src/app/components/tabs.tsx
@@ -1,25 +1,39 @@
 import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
 import Image from 'next/image';
 
-import Mobile from '@/icons/mobile.svg';
-import Tablet from '@/icons/tablet.svg';
-import Desktop from '@/icons/desktop.svg';
-export default function TabsBasicExample() {
+import Mobile from '@/img/mobile.svg';
+import Tablet from '@/img/tablet.svg';
+import Desktop from '@/img/desktop.svg';
+
+import DesktopPreview from '@/img/lazoo-desktop.png';
+import TabletPreview from '@/img/lazoo-tablet.png';
+import MobilePreview from '@/img/lazoo-mobile.png';
+
+import IosShare from '@/img/ios-share.svg';
+import IosSidebarLeft from '@/img/ios-sidebar-left.svg';
+import IosChevronRight from '@/img/ios-chevron-right.svg';
+import IosChevronLeft from '@/img/ios-chevron-left.svg';
+import IosTextSize from '@/img/ios-text-size.svg';
+import IosPuzzle from '@/img/ios-puzzle.svg';
+import IosRefresh from '@/img/ios-refresh.svg';
+import IosPlus from '@/img/ios-plus.svg';
+import IosTabs from '@/img/ios-tabs.svg';
+
+import Location from '@/img/location.svg';
+import dayjs from 'dayjs';
+
+import Device from './device';
+export default function DeviceTabs() {
 	const [basicActive, setBasicActive] = useState('tab1');
 
-	const handleBasicClick = (value: string) => {
-		if (value === basicActive) {
-			return;
-		}
-		setBasicActive(value);
-	};
-
+	let url = 'lazoo.org';
+	let title = 'LA Zoo website';
 	return (
 		<Tabs>
-			<TabList className="tabs tabs-boxed">
-				<Tab className="tab tab-active">
+			<TabList className="tabs tabs-boxed w-auto justify-center">
+				<Tab className="tab">
 					<span className="icon">
 						<Desktop className="block" />
 					</span>
@@ -40,33 +54,158 @@ export default function TabsBasicExample() {
 			</TabList>
 
 			<TabPanel>
-				<div className="mockup-browser border bg-base-300">
-					<div className="mockup-browser-toolbar">
-						<div className="input">https://lazoo.org</div>
-					</div>
-					<Image src="/lazoo-desktop.png" alt="Desktop view of LA Zoo Website" className="w-full" width={1140} height={810} />
+				<div className="flex justify-center items-center">
+					<Device
+						subtype="imac"
+						url={url}
+						type="desktop"
+						title={title}
+					>
+						<Image
+							src={DesktopPreview}
+							alt="Desktop view of LA Zoo Website"
+							className="w-full"
+							sizes="100vw"
+							loading="lazy"
+							style={{
+								width: '100%',
+								height: 'auto',
+							}}
+						/>
+					</Device>
 				</div>
 			</TabPanel>
 			<TabPanel>
-				<div className="relative flex justify-center w-[250px] h-[400px]border-8 border-black rounded-2xl">
-					<div className="absolute border-4  border-black h-full w-full"></div>
-					<span className="border border-black bg-black w-3 h-3 mt-2 rounded-full"></span>
-					<span className="absolute -right-2.5 top-20 border-2 border-black h-10 rounded-md"></span>
-				</div>
-			</TabPanel>
-			<TabPanel>
-				<div className="mockup-phone border-primary">
-					<div className="display">
-						<div className="mockup-browser mobile bg-base-300">
-							<div className="camera-wrapper">
-								<div className="camera"></div>
+				<div className="flex justify-center items-center">
+					<Device
+						subtype="ipad-pro"
+						url={url}
+						type="tablet"
+						title={title}
+					>
+						<Image
+							src={TabletPreview}
+							alt="Tablet view of LA Zoo Website"
+							className="w-full"
+							sizes="100vw"
+							loading="lazy"
+							style={{
+								width: '100%',
+								height: 'auto',
+							}}
+						/>
+					</Device>
+					{/* <div className="p-[3.5%] relative mx-auto border-gray-400 dark:border-gray-400 bg-stone-900 dark:bg-stone-900 border-[2px] rounded-[2.5rem] ">
+						<div className="h-[2px] w-[8%] bg-stone-900 dark:bg-stone-900 absolute right-[7%] -top-[4px] rounded-t-lg"></div>
+						<div className="h-[6%] w-[2px] bg-stone-900 dark:bg-stone-900 absolute -right-[5px] top-[15%] rounded-r-lg"></div>
+						<div className="h-[6%] w-[2px] bg-stone-900 dark:bg-stone-900 absolute -right-[5px] top-[23%] rounded-r-lg"></div>
+						<div className="rounded-[1rem] overflow-hidden aspect-tablet relative display w-[512px]">
+							<div className="absolute w-full h-full mockup-browser tablet bg-base-300 dark:bg-stone-700">
+								<div className="mockup-browser-toolbar grid grid-cols-[1fr_3fr_1fr]">
+									<div className="icons-left flex justify-around items-center">
+										<span className="icon text-blue-500 dark:text-blue-400">
+											<IosSidebarLeft />
+										</span>
+										<span className="icon text-blue-500 dark:text-blue-400">
+											<IosChevronLeft
+												style={{ height: '0.8em' }}
+											/>
+										</span>
+										<span className="icon text-blue-500 dark:text-blue-400">
+											<IosChevronRight
+												style={{ height: '0.8em' }}
+											/>
+										</span>
+									</div>
+									<div className="address-bar w-full !grid grid-cols-[1em_1em_1fr_1em_1em] gap-1 items-center rounded text-stone-800 dark:text-stone-100 text-center font-system bg-stone-50 dark:bg-stone-600">
+										<span className="icon">
+											<IosTextSize />
+										</span>
+										<div className="web-address col-start-3">
+											{url}
+										</div>
+
+										<span className="icon col-start-4">
+											<IosPuzzle />
+										</span>
+										<span className="icon col-start-5">
+											<IosRefresh
+												style={{ height: '0.9em' }}
+											/>
+										</span>
+									</div>
+
+									<div className="icons-right flex justify-around items-center">
+										<span className="icon text-blue-500 dark:text-blue-400">
+											<IosShare />
+										</span>
+										<span className="icon text-blue-500 dark:text-blue-400">
+											<IosPlus
+												style={{ height: '0.8em' }}
+											/>
+										</span>
+										<span className="icon text-blue-500 dark:text-blue-400">
+											<IosTabs />
+										</span>
+									</div>
+								</div>
+								<Image
+									src={TabletPreview}
+									alt="Tablet view of LA Zoo Website"
+									className="w-full"
+									sizes="100vw"
+									loading="lazy"
+									style={{
+										width: '100%',
+										height: 'auto',
+									}}
+								/>
 							</div>
-							<div className="mockup-browser-toolbar">
-								<div className="input">https://daisyui.com</div>
-							</div>
-							<Image src="/lazoo-mobile.png" alt="Mobile view of LA Zoo Website" className="w-full" width={900} height={1950} />
 						</div>
-					</div>
+					</div> */}
+
+					{/* <div className="mockup-tablet border-primary h-full max-h-full w-auto">
+						<div className="display">
+							<div className="mockup-browser mobile bg-base-300">
+								<div className="mockup-browser-toolbar">
+									<div className="input">{url}</div>
+								</div>
+								<Image
+									src={TabletPreview}
+									alt="Tablet view of LA Zoo Website"
+									className="w-full"
+									sizes="100vw"
+									loading="lazy"
+									style={{
+										width: '100%',
+										height: 'auto',
+									}}
+								/>
+							</div>
+						</div>
+					</div> */}
+				</div>
+			</TabPanel>
+			<TabPanel>
+				<div className="flex justify-center items-center">
+					<Device
+						subtype="iphone-14-pro"
+						url={url}
+						type="mobile"
+						title={title}
+					>
+						<Image
+							src={MobilePreview}
+							alt="Mobile view of LA Zoo Website"
+							className="w-full"
+							sizes="100vw"
+							loading="lazy"
+							style={{
+								width: '100%',
+								height: 'auto',
+							}}
+						/>
+					</Device>
 				</div>
 			</TabPanel>
 		</Tabs>
