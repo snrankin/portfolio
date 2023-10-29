e20463bcd55264dcd4a9f68ded59051d491fbf9c
Sam Rankin <samrankin.dev@gmail.com>
2023-10-28 20:07:08 -0700

    🎨 style(hero.tsx): update hero section content and layout 🖼️ feat(hero.tsx): add Pattern component to display a circuit image in the hero section
    The content and layout of the hero section have been updated. The heading now includes a greeting and the name of the author. The paragraph text has been modified to reflect the author's skills and expertise. Two buttons have been added to provide options for viewing the author's work and downloading their resume. Additionally, a new component called Pattern has been imported and added to display a circuit image in the hero section. These changes aim to improve the visual appeal and user experience of the hero section.

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,13 +1,21 @@
 'use client';
 import React, { useState } from 'react';
+import Pattern from '@/img/circuit-primary.svg';
 export default function Hero(): JSX.Element {
 	return (
 		<div className="hero min-h-screen bg-base-200">
-			<div className="hero-content text-center">
-				<div className="max-w-md">
-					<h1 className="text-5xl font-bold">Hello there</h1>
-					<p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-					<button className="btn btn-primary">Get Started</button>
+			<div className="hero-content w-full">
+				<div>
+					<h1 className="text-5xl font-bold">
+						Hello there, <br />
+						my name is Sam Rankin.
+					</h1>
+					<p className="py-6">I make the web beautiful and accessible across all screen sizes.</p>
+					<button className="btn btn-primary">See My Work</button>
+					<button className="btn btn-secondary">Download My Resume</button>
+				</div>
+				<div>
+					<Pattern />
 				</div>
 			</div>
 		</div>
