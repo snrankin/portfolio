4e3762c9905a960a1085cd35d014dcc77d18e066
Sam Rankin <samrankin.dev@gmail.com>
2023-10-28 20:07:20 -0700

    🔄 refactor(projects.tsx): rename TabsBasicExample component to DeviceTabs for better clarity and consistency The component TabsBasicExample has been renamed to DeviceTabs to provide a more descriptive and meaningful name. This improves code readability and maintains consistency with the naming conventions used in the project.

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -2,11 +2,11 @@
 import React, { useState } from 'react';
 
 import Section from './section';
-import TabsBasicExample from './tabs';
+import DeviceTabs from './tabs';
 export default function Projects(): JSX.Element {
 	return (
 		<Section id="projects" title="Featured Projects" displayTitle="projects" flag="featured">
-			<TabsBasicExample />
+			<DeviceTabs />
 		</Section>
 	);
 }
