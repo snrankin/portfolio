44f85e0cf8f06d677ffcf463c007f784014dbb0d
Sam Rankin <samrankin.dev@gmail.com>
2023-10-28 20:06:25 -0700

    🎨 style(about.tsx): Update About component layout and add profile picture 📸 chore(about.tsx): Add profile picture to About component for visual appeal and personal touch
    The About component layout has been updated to include a profile picture. The profile picture adds visual appeal and a personal touch to the About section. The profile picture is displayed using the `next/image` component and is loaded lazily for better performance. The layout of the component has been adjusted to accommodate the profile picture and the text content has been updated to provide a brief introduction and highlight the developer's passion for technology and commitment to creating beautiful, intuitive, and accessible web experiences.

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,22 +1,33 @@
 'use client';
 import React, { useState } from 'react';
-
+import Image from 'next/image';
 import Section from './section';
+import MeColor from '@/img/me-color.jpeg';
+import MeBW from '@/img/me-bw.png';
 export default function About(): JSX.Element {
 	return (
 		<Section id="about" title="About Me" displayTitle="about" flag="verbose">
-			<button className="btn">Button</button>
-			<button className="btn btn-neutral">Neutral</button>
-			<button className="btn btn-primary">Primary</button>
-			<button className="btn btn-secondary">Secondary</button>
-			<button className="btn btn-accent">Accent</button>
-
-			<button className="btn btn-info">Info</button>
-			<button className="btn btn-success">Success</button>
-			<button className="btn btn-warning">Warning</button>
-			<button className="btn btn-error">Error</button>
-			<button className="btn btn-ghost">Ghost</button>
-			<button className="btn btn-link">Link</button>
+			<div className="flex items-center prose">
+				<div className="xl:w-4/12 relative">
+					<Image
+						src={MeBW}
+						alt="Black and white profile picture of Sam Rankin"
+						className="w-full h-auto"
+						style={{
+							width: '100%',
+							height: 'auto',
+						}}
+						loading="lazy"
+					/>
+				</div>
+				<div>
+					<p>
+						Technology has always been a passion of mine. I love the constant change because I love learning new things and pushing the limits of what`&apos;`s possible. And with how much technology has become integral to our
+						society, I feel like I`&apos;`m part of something bigger when I create a website or a tool that helps a client, a coworker, or even myself in the future.
+					</p>
+					<p>As a web developer, I see it as my responsibility to make my work as beautiful, intuitive, and accessible as I possible can, no matter the screen size.</p>
+				</div>
+			</div>
 		</Section>
 	);
 }
