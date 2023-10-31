330c83722dd67935edfe6fe9088458bfc5e05187
Sam Rankin <samrankin.dev@gmail.com>
2023-10-30 19:00:23 -0700

    🎨 style(hero.tsx): refactor Hero component to use Section component for improved structure and readability The Hero component has been refactored to use the Section component, which provides a more structured and readable code. The Section component allows for better organization of the content within the Hero component, making it easier to understand and maintain.

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,23 +1,32 @@
 'use client';
 import React, { useState } from 'react';
-import Pattern from '@/img/circuit-primary.svg';
+import Section from './section';
 export default function Hero(): JSX.Element {
 	return (
-		<div className="hero min-h-screen bg-base-200">
+		<Section
+			id="hello"
+			title="Hello"
+			command="$(whoami)"
+			flags="include=tagline"
+			className="hero min-h-screen bg-base-200"
+		>
 			<div className="hero-content w-full">
-				<div>
+				<div className="text-center lg:prose-xl">
 					<h1 className="text-5xl font-bold">
-						Hello there, <br />
+						Hi there, <br />
 						my name is Sam Rankin.
 					</h1>
-					<p className="py-6">I make the web beautiful and accessible across all screen sizes.</p>
+					<p className="py-6">
+						{
+							"I'm not just about pixels and code. I'm about crafting digital spaces that feel right and bring a bit of joy. Let's team up and create some seriously awesome stuff together."
+						}
+					</p>
 					<button className="btn btn-primary">See My Work</button>
-					<button className="btn btn-secondary">Download My Resume</button>
-				</div>
-				<div>
-					<Pattern />
+					<button className="btn btn-secondary">
+						Download My Resume
+					</button>
 				</div>
 			</div>
-		</div>
+		</Section>
 	);
 }
