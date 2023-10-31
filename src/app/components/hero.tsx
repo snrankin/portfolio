e4fd464b29b4f9afe0a1825a3a25d05bd67e6ca5
Sam Rankin <samrankin.dev@gmail.com>
2023-10-31 01:23:07 -0700

    🎨 style(hero.tsx): improve layout and styling of the Hero component 💄 style(hero.tsx): update text alignment and font size in the Hero component 📝 chore(hero.tsx): update text content in the Hero component The layout and styling of the Hero component have been improved. The text alignment has been updated to be centered and the font size has been adjusted for better readability. The text content has also been updated to provide a more concise and engaging introduction. Additionally, the buttons in the Hero component have been wrapped in a flex container for better spacing and alignment.

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -11,20 +11,26 @@ export default function Hero(): JSX.Element {
 			className="hero min-h-screen bg-base-200"
 		>
 			<div className="hero-content w-full">
-				<div className="text-center lg:prose-xl">
+				<div className="text-center prose-xl">
 					<h1 className="text-5xl font-bold">
-						Hi there, <br />
-						my name is Sam Rankin.
+						<small>Hi there, my name is</small> <br />
+						<span className="text-pink-500">Sam Rankin</span>.
 					</h1>
-					<p className="py-6">
+					<h2>
+						I am a Phoenix, <abbr title="Arizona">AZ</abbr> based
+						full-stack web developer.
+					</h2>
+					<p className="py-6 text-lg">
 						{
 							"I'm not just about pixels and code. I'm about crafting digital spaces that feel right and bring a bit of joy. Let's team up and create some seriously awesome stuff together."
 						}
 					</p>
-					<button className="btn btn-primary">See My Work</button>
-					<button className="btn btn-secondary">
-						Download My Resume
-					</button>
+					<div className="flex flex-wrap gap-3 items-center justify-center">
+						<button className="btn btn-primary">See My Work</button>
+						<button className="btn btn-secondary">
+							Download My Resume
+						</button>
+					</div>
 				</div>
 			</div>
 		</Section>
