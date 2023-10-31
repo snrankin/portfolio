5def93995eed74c9684ff0437346940886a1298e
Sam Rankin <samrankin.dev@gmail.com>
2023-10-30 19:01:12 -0700

    🔧 fix(section.tsx): fix missing className prop in section component 🔧 fix(section.tsx): fix missing headingArgs prop in section component The section component was missing the className prop, which caused the styling to not be applied correctly. Additionally, the headingArgs prop was not being passed to the Heading component, resulting in missing title, command, argument, and flags. These fixes ensure that the section component behaves as expected and displays the correct heading and styling.

--- src/app/components/section.tsx
+++ src/app/components/section.tsx
@@ -1,15 +1,32 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, FunctionComponent, HTMLProps } from 'react';
 import { Waypoint } from 'react-waypoint';
 import Heading from './heading';
-export default function Section(props: any) {
+import classNames from 'classnames';
+import { pick, isEmpty } from 'lodash';
+
+export interface SectionProps
+	extends Omit<HTMLProps<HTMLElement>, 'className'> {
+	className?: string | undefined;
+	title?: string;
+	command?: string;
+	argument?: string;
+	flags?: string | string[];
+}
+export default function Section(props: SectionProps) {
 	let children: React.ReactNode = props.children;
+
+	let classes = classNames('py-32', 'min-h-screen', props.className);
+
+	let headingArgs = pick(props, ['title', 'command', 'argument', 'flags']);
 
 	return (
 		<Waypoint>
-			<section id={props.id} className="lg:py-32 min-h-screen">
+			<section className={classes} {...props}>
 				<div className="container mx-auto">
-					{props.showTitle ? <Heading title={props.title} displayTitle={props.displayTitle} flag={props.flag} /> : null}
+					{!isEmpty(props.title) || !isEmpty(props.command) ? (
+						<Heading {...headingArgs} />
+					) : null}
 
 					{children}
 				</div>
