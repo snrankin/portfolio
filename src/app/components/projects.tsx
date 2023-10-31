3ef2da6b7e51885ef4b0259a1cb3ddcce48d0fa3
Sam Rankin <samrankin.dev@gmail.com>
2023-10-31 01:23:58 -0700

    🔨 refactor(projects.tsx): rename DeviceTabs component to DeviceTabs to improve naming consistency 🔨 refactor(projects.tsx): import Image component from next/image to optimize image loading 🔨 refactor(projects.tsx): import necessary types and images for projects 🔨 refactor(projects.tsx): update project list with proper project objects and details 🔨 refactor(projects.tsx): update DeviceTabs component usage to pass project props 🔨 refactor(projects.tsx): update project card layout and content 🔨 refactor(projects.tsx): update project card actions to use Link and a tags 🔨 refactor(page.tsx): import necessary components and functions for LAZoo project page 🔨 refactor(page.tsx): update LAZoo project page layout and content 🔨 refactor(page.tsx): update DeviceTabs component usage to pass project props 🔨 refactor(page.tsx): update project highlights rendering in a list 🔨 refactor(page.tsx): update project highlights rendering to use getIcon function 🔨 refactor(page.tsx): update project summary rendering 🔨 refactor(page.tsx): update project name rendering 🔨 refactor(page.tsx): update project highlights rendering to use flex-wrap for better layout
    The changes were made to improve the code readability and maintainability. The component names were updated to follow consistent naming conventions. The imports were optimized to only import necessary components and functions. The project list was updated with proper project objects and details. The project card layout and content were updated to display the project information correctly. The project card actions were updated to use Link and a tags for better navigation. The LAZoo project page layout and content were updated to match the new project card layout. The project highlights and summary were updated to render correctly.

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,8 +1,68 @@
 'use client';
 import React, { useState } from 'react';
-
+import { paramCase } from 'change-case-all';
 import Section from './section';
-import DeviceTabs from './tabs';
+import DeviceTabs from './device-tabs';
+import Image from 'next/image';
+import { getIcon } from '../lib/utils';
+import {
+	StaticImageData,
+	StaticImport,
+} from 'next/dist/shared/lib/get-img-props';
+
+import LAZooDesktop from '@/img/lazoo-desktop.png';
+import LAZooTablet from '@/img/lazoo-tablet.png';
+import LAZooMobile from '@/img/lazoo-mobile.png';
+import Link from 'next/link';
+export type Project = {
+	name: string;
+	slug: string;
+	startDate?: string | Date;
+	endDate?: string | Date;
+	summary?: string;
+	url?: string;
+	repo?: string;
+	image?: StaticImport | string;
+	highlights?: string[];
+	desktop?: StaticImport | string;
+	tablet?: StaticImport | string;
+	mobile?: StaticImport | string;
+};
+
+export type ProjectListType = {
+	[key: string]: Project;
+};
+
+export const ProjectList: ProjectListType = {
+	lazoo: {
+		name: 'LA Zoo Website Redesign',
+		slug: 'lazoo',
+		startDate: '2020-02-28',
+		endDate: '2021-03-31',
+		summary:
+			'Complete redesign of the LA Zoo website. Built using Sage 9 Laravel Wordpress theme, Advanced Custom Fields and custom Gutenberg blocks',
+		url: 'https://www.lazoo.org/',
+		image: LAZooDesktop,
+		highlights: ['WordPress', 'Laravel', 'PHP', 'React', 'Webpack'],
+		desktop: LAZooDesktop,
+		tablet: LAZooTablet,
+		mobile: LAZooMobile,
+	},
+	'wp-readme': {
+		name: 'WP ReadMe Generator',
+		slug: 'lazoo',
+		startDate: '2020-02-28',
+		endDate: '2021-03-31',
+		summary:
+			'Complete redesign of the LA Zoo website. Built using Sage 9 Laravel Wordpress theme, Advanced Custom Fields and custom Gutenberg blocks',
+		repo: 'https://github.com/snrankin/generate-wp-readme',
+		url: 'https://www.npmjs.com/package/@snrankin/generate-wp-readme',
+		highlights: ['TypeScript', 'NodeJS', 'WordPress', 'JavaScript'],
+		desktop: LAZooDesktop,
+		tablet: LAZooTablet,
+		mobile: LAZooMobile,
+	},
+};
 export default function Projects(): JSX.Element {
 	return (
 		<Section
@@ -11,8 +71,56 @@ export default function Projects(): JSX.Element {
 			command="ls"
 			argument="projects"
 			flags="featured"
+			className="bg-base-200"
 		>
-			<DeviceTabs />
+			<div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+				{Object.values(ProjectList).map((project: Project, slug) => {
+					return (
+						<div
+							key={slug}
+							className="card card-compact bg-base-100 shadow"
+						>
+							<div className="card-body">
+								<h3 className="card-title">{project.name}</h3>
+								<p>{project.summary}</p>
+								{project.highlights != undefined ? (
+									<ul className="inline-flex list-none gap-3 p-0 flex-wrap">
+										{project.highlights?.map((h, i) => {
+											return (
+												<li
+													key={i}
+													className="badge gap-1"
+												>
+													{getIcon(h)}
+													{h}
+												</li>
+											);
+										})}
+									</ul>
+								) : null}
+
+								<div className="card-actions justify-end">
+									<Link
+										href={`/projects/${slug}`}
+										className="btn btn-link"
+									>
+										View More
+									</Link>
+									{project.repo != undefined ? (
+										<a
+											href={project.repo}
+											target="_blank"
+											className="btn btn-link"
+										>
+											View Repo
+										</a>
+									) : null}
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</Section>
 	);
 }
