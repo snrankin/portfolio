d6fc57e1beffd9c3c9c78a69698444f871f027e2
Sam Rankin <samrankin.dev@gmail.com>
2023-10-30 19:00:10 -0700

    🔧 fix(heading.tsx): refactor Heading component to improve readability and add support for multiple flags 💡 refactor(heading.tsx): improve the structure and readability of the Heading component. Add support for multiple flags in the command. The Heading component has been refactored to improve readability and maintainability. The component now accepts a `HeadingProps` interface which defines the expected props. The `flags` prop can now accept either a string or an array of strings, allowing for multiple flags to be displayed in the command. The component also uses the `padStart` function from the lodash library to add padding to each flag. This change improves the flexibility and extensibility of the Heading component.

--- src/app/components/heading.tsx
+++ src/app/components/heading.tsx
@@ -1,14 +1,45 @@
 'use client';
-import React from 'react';
-export default function Heading(props: any): JSX.Element {
+import React, { useEffect, useRef } from 'react';
+import { padStart } from 'lodash';
+
+export interface HeadingProps {
+	title?: string;
+	command?: string;
+	argument?: string;
+	flags?: string | string[];
+}
+export default function Heading(props: HeadingProps): JSX.Element {
+	let flags = props.flags;
+
+	if (flags != undefined) {
+		if (typeof flags == 'string') {
+			flags = flags.split(' | ');
+		}
+		flags = flags.map((flag) => {
+			flag = padStart(flag, flag.length + 2, '-');
+			console.log(flag);
+			return flag;
+		});
+		flags = flags.join(' ');
+	}
+
 	return (
-		<h2 className="mockup-code lg:w-9/12 mx-auto">
-			<span className="sr-only">{props.title}</span>
-			<pre data-prefix="$" aria-hidden="true" className="font-code">
-				<code className="text-info">ls </code>
-				<code>{props.displayTitle}</code>
-				{props.flag ? <code className="text-secondary"> --{props.flag}</code> : null}
-			</pre>
+		<h2 className="flex justify-center">
+			<span className="min-w-[300px] mockup-code lg:w-max mx-auto dark:bg-gray-800">
+				{props.title != undefined ? (
+					<span className="sr-only">{props.title} </span>
+				) : null}
+
+				<pre data-prefix="$" aria-hidden="true" className="font-mono">
+					{props.command != undefined ? (
+						<code className="text-lime-500">{props.command} </code>
+					) : null}
+					{props.argument != undefined ? props.argument : null}
+					{flags != undefined ? (
+						<code className="text-secondary"> {flags}</code>
+					) : null}
+				</pre>
+			</span>
 		</h2>
 	);
 }
