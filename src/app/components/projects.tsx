a8364eb2dfe37c3bbd75919838ca421ff84d0001
Sam Rankin <samrankin.dev@gmail.com>
2023-10-30 19:00:56 -0700

    🔧 chore(projects.tsx): add command, argument, and flags props to Section component for better customization The Section component in the Projects component now accepts additional props: command, argument, and flags. This allows for better customization and flexibility when rendering the Section component.

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -5,7 +5,13 @@ import Section from './section';
 import DeviceTabs from './tabs';
 export default function Projects(): JSX.Element {
 	return (
-		<Section id="projects" title="Featured Projects" displayTitle="projects" flag="featured">
+		<Section
+			id="projects"
+			title="Featured Projects"
+			command="ls"
+			argument="projects"
+			flags="featured"
+		>
 			<DeviceTabs />
 		</Section>
 	);
