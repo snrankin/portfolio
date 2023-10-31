82d36006259a4768dbb8852ad5cba3119ded190d
Sam Rankin <samrankin.dev@gmail.com>
2023-10-31 01:21:39 -0700

    🔨 refactor(about.tsx): update layout and remove commented out code for profile picture 📝 chore(about.tsx): update about section text for improved clarity and readability The layout of the About component has been updated to use a grid layout for better responsiveness. The commented out code for the profile picture has been removed as it is no longer needed. The text in the about section has been revised to provide clearer and more concise information about the developer's experience and interests. The redundant paragraph about crafting digital spaces has been removed to streamline the content.

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -12,23 +12,37 @@ export default function About(): JSX.Element {
 			command="$(whoami)"
 			flags="verbose"
 		>
-			<div className="flex items-center prose">
-				<div className="xl:w-4/12 relative">
-					<Image
-						src={MeBW}
-						alt="Black and white profile picture of Sam Rankin"
-						className="w-full h-auto"
-						style={{
-							width: '100%',
-							height: 'auto',
-						}}
-						loading="lazy"
-					/>
+			<div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] items-center gap-4">
+				<div className="relative">
+					<div className="grid grid-cols-9 grid-rows-9">
+						{/* <div className="border relative overflow-hidden col-span-9 row-span-9 md:row-span-4 md:col-span-4 row-start-1 col-start-1 aspect-h-1 aspect-w-1">
+							<svg
+								className="w-10 h-10 text-gray-200 dark:text-gray-600"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 20 18"
+								style={{ width: '100%', height: 'auto' }}
+							>
+								<path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+							</svg>
+						</div> */}
+						<div className="border relative overflow-hidden col-span-9 row-span-9  aspect-h-1 aspect-w-1">
+							<Image
+								src={MeBW}
+								alt="Black and white profile picture of Sam Rankin"
+								className="w-full h-auto !m-0"
+								loading="lazy"
+								style={{ width: '100%', height: 'auto' }}
+							/>
+							{/* <div className="absolute w-full left-0 top-auto bottom-[-1px] h-[40%] bg-gradient-to-t from-base-100"></div> */}
+						</div>
+					</div>
 				</div>
-				<div>
+				<div className="prose max-w-none">
 					<p>
 						{
-							"I'm a full-stack developer navigating the ever-changing landscape of the web since 2010. My journey has had me mostly focused on WordPress, but I'm confident in my ability to pick up any platform or language."
+							"I've been immersed in the ever-changing landscape of the digital realm (professionally) since 2010. My journey has had me mostly focused on WordPress, but I'm confident in my ability to pick up any platform or language."
 						}
 					</p>
 
@@ -41,12 +55,6 @@ export default function About(): JSX.Element {
 					<p>
 						{
 							"When I'm not diving into code, you’ll find me nose-deep in a book, spreading smiles as a therapy dog team with my corgi, Zoey, catching the excitement at Arizona Diamondbacks games, or getting my hands messy in art classes."
-						}
-					</p>
-
-					<p>
-						{
-							"I'm not just about pixels and code. I'm about crafting digital spaces that feel right and bring a bit of joy. Let's team up and create some seriously awesome stuff together."
 						}
 					</p>
 				</div>
