141e05223a08a644873903c63d391011ec910256
Sam Rankin <samrankin.dev@gmail.com>
2023-11-04 11:21:16 -0700

    feat: adding icon component

---
+++ src/app/components/icons/icon-swap.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import Icon from './icon';
+import React, { useState } from 'react';
+import { isEmpty, omit, set } from 'lodash';
+import classNames from 'classnames';
+interface IconSwapProps extends React.ComponentPropsWithoutRef<'button'> {
+	iconOn: string;
+	iconOff: string;
+	groupOn?: 'web' | 'dev' | 'ios';
+	groupOff?: 'web' | 'dev' | 'ios';
+	swapAction?: 'hover' | 'click';
+	swap?: 'rotate' | 'flip';
+}
+
+export default function IconSwap(props: IconSwapProps): JSX.Element {
+	const [isActive, setActive] = useState(false);
+	let groupOn = props.groupOn != undefined ? props.groupOn : 'web';
+	let groupOff = props.groupOff != undefined ? props.groupOff : 'web';
+	let swapAction = props.swapAction != undefined ? props.swapAction : 'click';
+	let swap = props.swap != undefined ? props.swap : 'flip';
+
+	let classes = classNames('swap', `swap-${swap}`, props.className, {
+		'swap-active': isActive,
+	});
+
+	let itemProps = omit(props, [
+		'iconOn',
+		'iconOff',
+		'groupOn',
+		'groupOff',
+		'swapAction',
+		'swap',
+	]);
+
+	const handleSwap = () => {
+		if (isActive) {
+			setActive(false);
+		}
+		setActive(true);
+	};
+
+	set(itemProps, 'className', classes);
+
+	if (swapAction == 'click') {
+		set(itemProps, 'onClick', handleSwap);
+	}
+
+	if (swapAction == 'hover') {
+		set(itemProps, 'onMouseEnter', handleSwap);
+	}
+
+	return (
+		<>
+			{!isEmpty(props.iconOff) && !isEmpty(props.iconOn) ? (
+				<button {...itemProps}>
+					<Icon
+						group={groupOff}
+						icon={props.iconOff}
+						className="swap-off"
+					/>
+					<Icon
+						group={groupOn}
+						icon={props.iconOn}
+						className="swap-on"
+					/>
+				</button>
+			) : null}
+		</>
+	);
+}
---
+++ src/app/components/icons/link.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import { hasIcon, getIcon } from '@/app/lib/icons';
+
+import React, { createContext, useContext, HTMLProps } from 'react';
+import { get, isEmpty, startsWith, omit, set } from 'lodash';
+import classNames from 'classnames';
+import { simplifyUrl } from '@/app/lib/utils';
+
+import { IconConfig, IIconContext } from './icon';
+export interface IconLinkProps
+	extends IconConfig,
+		Omit<HTMLProps<HTMLAnchorElement>, 'title'> {
+	displayUrl?: boolean;
+}
+
+export type IIconLinkContext = IIconContext & {
+	displayUrl?: boolean;
+};
+
+export const IconLinkContextConfig: IIconLinkContext = {
+	group: 'web',
+	titleDisplay: 'hidden',
+	titleClasses: '',
+	iconClasses: '',
+	displayUrl: false,
+};
+
+export const IconLinkContext = createContext(IconLinkContextConfig);
+
+export default function Icon(props: IconLinkProps): JSX.Element {
+	let { group, titleDisplay, titleClasses, iconClasses, displayUrl } = props;
+	let iconName = props.icon != undefined ? props.icon : '';
+	const HAS_ICON = hasIcon(iconName, group);
+
+	let icon;
+
+	let classes = classNames(
+		{
+			'items-center':
+				titleDisplay == 'inline' && !isEmpty(props.title) && HAS_ICON,
+			'gap-1':
+				titleDisplay == 'inline' && !isEmpty(props.title) && HAS_ICON,
+			'inline-flex':
+				titleDisplay == 'inline' && !isEmpty(props.title) && HAS_ICON,
+			tooltip: titleDisplay == 'popover' && HAS_ICON,
+		},
+		props.className
+	);
+
+	let iconProps: HTMLProps<HTMLSpanElement> = {
+		'aria-hidden': 'true',
+		role: 'presentation',
+		className: classNames('icon', iconClasses),
+	};
+
+	titleClasses = classNames(
+		'icon-label',
+		{
+			'sr-only':
+				(titleDisplay == 'hidden' || titleDisplay == 'popover') &&
+				HAS_ICON,
+		},
+		titleClasses
+	);
+
+	let attr = omit(props, [
+		'titleDisplay',
+		'titleClasses',
+		'title',
+		'group',
+		'icon',
+		'iconClasses',
+	]);
+
+	set(attr, 'className', classes);
+
+	let link,
+		title = props.title != undefined ? props.title : '';
+
+	if (props.href != undefined && displayUrl) {
+		title = simplifyUrl(props.href);
+	}
+
+	if (titleDisplay == 'popover' && HAS_ICON) {
+		set(attr, 'data-tip', title);
+	}
+
+	if (!isEmpty(iconName)) {
+		icon = getIcon(iconName, group);
+	}
+	return (
+		<>
+			{!isEmpty(iconName) && (
+				<a {...attr}>
+					<span {...iconProps}>{icon}</span>
+					{!isEmpty(props.title) && (
+						<span className={titleClasses}>{title}</span>
+					)}
+				</a>
+			)}
+		</>
+	);
+}
