7dacef86ca8c0caf7d4568d9f5ec12097b3ce7bc
Sam Rankin <samrankin.dev@gmail.com>
2023-11-04 11:27:29 -0700

    chore(next.config.js): refactor SVG handling in webpack configuration
    The existing rule for handling SVG imports in the webpack configuration has been refactored to improve clarity and flexibility. The changes include:
    
    - Reusing the existing rule that handles SVG imports and modifying it to only apply to SVG imports ending in "?url".
    - Adding a new rule to convert all other SVG imports to React components using "@svgr/webpack".
    - Adding a new rule to handle SVG imports with the query parameter "raw" and treat them as asset sources.
    - Modifying the file loader rule to exclude SVG files, as they are now handled separately.
    
    These changes improve the handling of SVG imports in the project and provide more flexibility for different use cases.

--- next.config.js
+++ next.config.js
@@ -12,18 +12,36 @@ const nextConfig = {
 		],
 	},
 	webpack: (config) => {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			resourceQuery: { not: [/raw/] },
-			use: ['@svgr/webpack'],
-		});
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			resourceQuery: /raw/,
-			type: 'asset/source',
-		});
+		// Grab the existing rule that handles SVG imports
+		const fileLoaderRule = config.module.rules.find((rule) =>
+			rule.test?.test?.('.svg')
+		);
+
+		config.module.rules.push(
+			// Reapply the existing rule, but only for svg imports ending in ?url
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, // *.svg?url
+			},
+			// Convert all other *.svg imports to React components
+			{
+				test: /\.svg$/i,
+				issuer: /\.[jt]sx?$/,
+				resourceQuery: { not: /(url|raw)/ }, // exclude if *.svg?url
+				use: ['@svgr/webpack'],
+			},
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /raw/,
+				type: 'asset/source',
+			}
+		);
+
+		// Modify the file loader rule to ignore *.svg, since we have it handled now.
+		fileLoaderRule.exclude = /\.svg$/i;
+
 		return config;
 	},
 };
