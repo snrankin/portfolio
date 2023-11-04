8346f03cd4ef6a3fca419040f23188ebd170995b
Sam Rankin <samrankin.dev@gmail.com>
2023-11-04 11:24:57 -0700

    style(not-found.jsx): improve readability by adding line breaks and indentation to JSX code fix(not-found.jsx): fix broken link by changing href value to "/"

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,11 +4,15 @@ export default function NotFound() {
 			<div className="text-center">
 				<h1 className="font-black text-9xl">404</h1>
 
-				<p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
+				<p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+					Uh-oh!
+				</p>
 
-				<p className="mt-4 text-gray-500">We can`&apos;`t find that page.</p>
+				<p className="mt-4 text-gray-500">
+					We can`&apos;`t find that page.
+				</p>
 
-				<a href="#" className="btn btn-primary">
+				<a href="/" className="btn btn-primary">
 					Go Back Home
 				</a>
 			</div>
