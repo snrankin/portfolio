93413672c00ad8ca09322ff9354df92e006bf7c2
Sam Rankin <samrankin.dev@gmail.com>
2023-11-04 11:23:42 -0700

    feat: adding print version of timeline

---
+++ src/app/components/timeline/print-group.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React, { HTMLProps } from 'react';
+import { jobs } from '@/app/lib/jobs';
+import TimelineItem from './print-item';
+export default function Timeline(
+	props: HTMLProps<HTMLDivElement>
+): JSX.Element {
+	return (
+		<div {...props}>
+			{jobs.map((job, i) => {
+				return <TimelineItem key={i} {...job} />;
+			})}
+		</div>
+	);
+}
