8563f32b1040ccd1bc40cfa33d067c773194f152
Sam Rankin <samrankin.dev@gmail.com>
2023-11-05 11:32:28 -0700

    refactor(home page): restructure page layout to include header and footer components
    The changes in this commit restructure the layout of the home page to include a header and footer component. This improves the overall design and user experience of the page by providing consistent navigation and branding elements. The header component is added at the top of the page, followed by the main content section which includes the existing components: Hero, About, Skills, Projects, and WorkHistory. Finally, the footer component is added at the bottom of the page.

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -3,14 +3,20 @@ import About from './about';
 import Skills from './skills';
 import Projects from './projects';
 import WorkHistory from './work-history';
+import Header from '@/app/components/global/header';
+import Footer from '@/app/components/global/footer';
 export default function Page() {
 	return (
 		<>
-			<Hero />
-			<About />
-			<Skills />
-			<Projects />
-			<WorkHistory />
+			<Header />
+			<main className="flex min-h-screen flex-col w-full overflow-y-none">
+				<Hero />
+				<About />
+				<Skills />
+				<Projects />
+				<WorkHistory />
+			</main>
+			<Footer />
 		</>
 	);
 }
