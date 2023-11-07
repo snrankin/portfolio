6921c1422b7476695c8f4d345ffbc00bc6077ed2
Sam Rankin <samrankin.dev@gmail.com>
2023-11-06 19:22:54 -0700

    chore(about.tsx): update CSS class names for grid and image to improve responsiveness chore(about.tsx): update CSS class name for image frame to adjust stroke width on different screen sizes chore(about.tsx): update CSS class name for prose to adjust font size on larger screens

--- src/app/(pages)/(home)/about.tsx
+++ src/app/(pages)/(home)/about.tsx
@@ -13,22 +13,22 @@ export default function About(): JSX.Element {
 			command="$(whoami)"
 			flags="verbose"
 		>
-			<div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] items-center gap-11">
+			<div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] items-center gap-11">
 				<div className="relative">
 					<div className="aspect-w-1 aspect-h-1">
 						<Image
 							src={MeImg}
 							alt="Profile picture of Sam Rankin"
-							className="w-full h-auto mask mask-diamond grayscale"
+							className="w-full h-auto mask mask-diamond grayscale will-change-auto hover:grayscale-0"
 							loading="lazy"
 							fill={true}
 						/>
 					</div>
 					<div className="absolute w-full h-full inset-0 flex">
-						<ImgFrame classNames="block w-full h-full grow" />
+						<ImgFrame className="block w-full h-full grow stroke-[20px] md:stroke-[15px]" />
 					</div>
 				</div>
-				<div className="prose-lg max-w-none">
+				<div className="prose lg:prose-lg max-w-none">
 					{Me.about?.map((p, i) => (
 						<p key={i}>{p}</p>
 					))}
