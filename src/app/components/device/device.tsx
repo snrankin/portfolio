9d20e450cf6450651aa09e270698a28777dadb1b
Sam Rankin <samrankin.dev@gmail.com>
2023-11-06 19:24:33 -0700

    refactor(device): moving devices component to it's own folder

---
+++ src/app/components/device/device.tsx
@@ -0,0 +1,121 @@
+'use client';
+import React, { HTMLProps } from 'react';
+import classNames from 'classnames';
+import Icon from '../icons/icon';
+import './devices.css';
+import BrowserToolbar from './toolbar';
+import DesktopFrame from '@/img/desktop.svg';
+import LaptopFrame from '@/img/laptop.svg';
+import TabletFrame from '@/img/tablet.svg';
+import MobileFrame from '@/img/mobile.svg';
+import { omit, pick, set } from 'lodash';
+
+export interface DeviceProps extends HTMLProps<HTMLDivElement> {
+	type: 'desktop' | 'laptop' | 'tablet' | 'mobile';
+	url?: string;
+	title?: string;
+	children?: React.ReactNode;
+}
+
+export default function Device(props: DeviceProps): JSX.Element {
+	var wrapperClasses = classNames(props.className);
+
+	let atts = omit(props, ['type', 'url', 'title', 'children']);
+
+	set(atts, 'className', wrapperClasses);
+
+	var deviceClasses = classNames(
+		'device',
+		`device-${props.type}`,
+		'w-full',
+		'relative',
+		'z-10',
+		'block',
+		'min-w-full',
+		'h-full'
+	);
+
+	var screenClasses = classNames(
+		'screen',
+		'relative',
+		'overflow-hidden',
+		'display',
+		'rounded-0',
+		'w-full',
+		'bg-white',
+		{
+			'aspect-w-16': props.type == 'desktop',
+			'aspect-h-9': props.type == 'desktop',
+			'aspect-w-3': props.type == 'laptop',
+			'aspect-h-2': props.type == 'laptop',
+			'aspect-w-[768]': props.type == 'tablet',
+			'aspect-h-[1024]': props.type == 'tablet',
+			'aspect-w-[59]': props.type == 'mobile',
+			'aspect-h-[126]': props.type == 'mobile',
+		}
+	);
+
+	var displayClasses = classNames(
+		'flex',
+		'w-full',
+		'absolute',
+		'rounded-0',
+		'left-0',
+		'top-0',
+		'w-full',
+		{
+			'px-[7.361%]': props.type == 'laptop',
+			'p-[5.247%]': props.type == 'tablet',
+			'p-[4.677%]': props.type == 'mobile',
+			'p-[2.6%]': props.type == 'desktop',
+		}
+	);
+
+	let aspectClasses = classNames(
+		'screen',
+		'relative',
+		'overflow-hidden',
+		'display',
+		'rounded-0',
+		'w-full',
+		'bg-white',
+		{
+			'aspect-tablet': props.type == 'tablet',
+			'aspect-mobile': props.type == 'mobile',
+		}
+	);
+
+	let children: React.ReactNode = props.children;
+	return (
+		<div {...atts}>
+			<div className="relative w-full h-full">
+				<div className={displayClasses}>
+					<div className={screenClasses}>
+						<div
+							className={`absolute top-0 left-0 w-full h-full  mockup-browser ${props.type} !rounded-none`}
+						>
+							{props.url != undefined && (
+								<BrowserToolbar {...props} />
+							)}
+
+							{children}
+						</div>
+					</div>
+				</div>
+
+				{props.type == 'mobile' && (
+					<MobileFrame className={deviceClasses} />
+				)}
+				{props.type == 'tablet' && (
+					<TabletFrame className={deviceClasses} />
+				)}
+				{props.type == 'laptop' && (
+					<LaptopFrame className={deviceClasses} />
+				)}
+				{props.type == 'desktop' && (
+					<DesktopFrame className={deviceClasses} />
+				)}
+			</div>
+		</div>
+	);
+}
---
+++ src/app/components/device/toolbar.tsx
@@ -0,0 +1,84 @@
+'use client';
+import React from 'react';
+import classNames from 'classnames';
+import Icon from '../icons/icon';
+import { DeviceProps } from './device';
+
+export const WindowButtons = () => (
+	<div className="window-icons inline-flex gap-1 justify-around items-center">
+		<span className="w-[0.8em] h-[0.8em] bg-danger-500 rounded-full"></span>
+		<span className="w-[0.8em] h-[0.8em] bg-warning-500 rounded-full"></span>
+		<span className="w-[0.8em] h-[0.8em] bg-success-500 rounded-full"></span>
+	</div>
+);
+
+export default function BrowserToolbar(props: DeviceProps) {
+	let rightIconClasses = classNames(
+		'icons-right',
+		'flex',
+		'items-center',
+		'gap-2',
+		{
+			'justify-around': props.type != 'desktop',
+			'justify-end': props.type == 'desktop',
+		}
+	);
+
+	let leftIconClasses = classNames(
+		'icons-left',
+		'flex',
+		'items-center',
+		'gap-2',
+		{
+			'justify-around': props.type != 'desktop',
+			'justify-start': props.type == 'desktop',
+		}
+	);
+
+	let wrapperClasses = classNames(
+		'mockup-browser-toolbar',
+		'text-blue-500',
+		'dark:text-blue-400',
+		'text-sm',
+		'w-full',
+		'!px-[3%]',
+		{
+			'!grid': props.type != 'mobile',
+			'grid-cols-[1fr_3fr_1fr]': props.type != 'mobile',
+		}
+	);
+
+	return (
+		<div className={wrapperClasses}>
+			{props.type != 'mobile' && (
+				<div className={leftIconClasses}>
+					{props.type == 'desktop' ||
+						(props.type == 'laptop' && <WindowButtons />)}
+					{/* <Icon icon="sidebar" group="ios" />
+					<div className="flex justify-around items-center">
+						<Icon icon="chevron-left" group="ios" />
+						<Icon icon="chevron-right" group="ios" />
+					</div> */}
+				</div>
+			)}
+			<div className="address-bar w-full !grid grid-cols-1 gap-1 items-center rounded text-stone-800 dark:text-stone-100 text-center font-system bg-stone-50 dark:bg-stone-600">
+				{/* {props.type == 'desktop' ? (
+					<Icon icon="search" group="ios" />
+				) : (
+					<Icon icon="textResize" group="ios" />
+				)}
+				{props.type == 'mobile' && <Icon icon="puzzle" group="ios" />} */}
+				<div className="web-address">{props.url}</div>
+				{/* {props.type == 'tablet' && <Icon icon="puzzle" group="ios" />}
+				<Icon icon="refresh" group="ios" /> */}
+			</div>
+			{/* {props.type != 'mobile' && (
+				<div className={rightIconClasses}>
+					<Icon icon="share" group="ios" />
+					<Icon icon="plus" group="ios" />
+					<Icon icon="tabs" group="ios" />
+				</div>
+			)} */}
+		</div>
+	);
+}
