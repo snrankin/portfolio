4a82ae5c799fad6f58ab7f3178b5f23bbf898721
Sam Rankin <samrankin.dev@gmail.com>
2023-11-07 20:28:02 -0700

    fix(section.tsx): removing min-height class

--- src/app/components/section/section.tsx
+++ src/app/components/section/section.tsx
@@ -12,12 +12,7 @@ export default function Section(props: SectionProps) {
 	let args = props;
 	let children: React.ReactNode = props.children;
 
-	let classes = classNames(
-		'py-12',
-		'xl:py-32',
-		'min-h-screen',
-		props.className
-	);
+	let classes = classNames('py-12', 'xl:py-32', props.className);
 
 	let headingArgs = pick(props, ['title', 'command', 'argument', 'flags']);
 	let sectionProps = omit(props, [
