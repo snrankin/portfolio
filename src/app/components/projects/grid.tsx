831c3c12797acdb1c379cbb6458c37ae132b95ee
Sam Rankin <samrankin.dev@gmail.com>
2023-11-08 22:00:43 -0700

    fix(grid.tsx): remove unused import statements to improve code cleanliness feat(grid.tsx): add support for passing projects as a string or an array to filter and display specific projects
    fix(item.tsx): remove unused import statements to improve code cleanliness
    feat(item.tsx): refactor Project component to remove unnecessary props and improve code readability
    
    fix(list.tsx): remove unused import statements to improve code cleanliness
    feat(list.tsx): refactor ProjectsGroup component to remove unnecessary props and improve code readability

--- src/app/components/projects/grid.tsx
+++ src/app/components/projects/grid.tsx
@@ -3,17 +3,27 @@ import React, { HTMLProps } from 'react';
 import { IProject } from '@/app/lib/interfaces';
 import { ProjectsList } from '@/app/lib/projects';
 import Project, { ProjectCardProps } from './card';
-import { merge, omit } from 'lodash';
+import { filter, indexOf, isString, merge, omit, pick, set } from 'lodash';
 
 interface ProjectGroupProps extends HTMLProps<HTMLDivElement> {
 	projectProps?: ProjectCardProps;
+	projects: string | string[];
 }
 
 export default function ProjectsGroup(props: ProjectGroupProps): JSX.Element {
-	var atts = omit(props, ['projectProps']);
+	var atts = omit(props, ['projectProps', 'projects']);
+
+	let projects = isString(props.projects)
+		? props.projects.split(',')
+		: props.projects;
+
+	let projectList = filter(
+		ProjectsList,
+		(i: IProject) => indexOf(projects, i.slug) != -1
+	);
 	return (
 		<div {...atts}>
-			{ProjectsList.map((project: IProject) => {
+			{projectList.map((project: IProject) => {
 				merge(project, props.projectProps);
 				return <Project key={project.slug} {...project} />;
 			})}
--- src/app/components/projects/item.tsx
+++ src/app/components/projects/item.tsx
@@ -9,91 +9,25 @@ import Link from 'next/link';
 import { IProject, IProjectLinks } from '@/app/lib/interfaces';
 import { paramCase } from 'change-case-all';
 import IconLink from '../icons/link';
-export interface ProjectCardProps extends CardProps, IProject {}
 
-export function ProjectLinks(links: IProjectLinks) {
-	let { github, bitbucket, npm, website } = links;
-	return (
-		<>
-			{github != undefined && (
-				<IconLink
-					href={github}
-					group="dev"
-					icon="github"
-					title={simplifyUrl(github).replace('github.com/', '')}
-					titleDisplay="inline"
-					target="_blank"
-				/>
-			)}
-			{bitbucket != undefined && (
-				<IconLink
-					href={bitbucket}
-					group="dev"
-					icon="bitbucket"
-					title={simplifyUrl(bitbucket).replace('bitbucket.com/', '')}
-					titleDisplay="inline"
-					target="_blank"
-				/>
-			)}
-			{npm != undefined && (
-				<IconLink
-					href={npm}
-					group="dev"
-					icon="npm"
-					title={simplifyUrl(npm).replace('npmjs.com/package/', '')}
-					titleDisplay="inline"
-					target="_blank"
-				/>
-			)}
-		</>
-	);
-}
-export default function Project(props: ProjectCardProps) {
+export default function Project(props: IProject) {
 	let { startDate, endDate, summary, url, repo } = props;
-
-	let attr: CardProps = omit(props, [
-		'slug',
-		'image',
-		'startDate',
-		'shortTitle',
-		'endDate',
-		'summary',
-		'url',
-		'repo',
-		'highlights',
-		'desktop',
-		'tablet',
-		'mobile',
-		'links',
-	]);
-
-	let subtitle = displayDate(startDate, endDate);
 
 	let title = props.title != undefined ? props.title : '';
 
-	let displayTitle = props.shortTitle != undefined ? props.shortTitle : title;
-
-	set(attr, 'title', displayTitle);
-
-	let slug = props.slug != undefined ? props.slug : paramCase(displayTitle);
-	let classnames = classNames(props.className, `project-${slug}`);
-
-	set(attr, 'className', classnames);
-
 	return (
-		<Card {...attr}>
-			<p>
-				{url != undefined ? (
-					<a href={url} target="_blank">
-						{simplifyUrl(url)}
-					</a>
-				) : null}
-				{repo != undefined && url == undefined ? (
-					<a href={repo} target="_blank">
-						{simplifyUrl(url)}
-					</a>
-				) : null}
-			</p>
-		</Card>
+		<li>
+			<span className="block">{title}</span>
+			{url != undefined ? (
+				<a href={url} target="_blank" className="block">
+					{simplifyUrl(url)}
+				</a>
+			) : null}
+			{repo != undefined && url == undefined ? (
+				<a href={repo} target="_blank" className="block">
+					{simplifyUrl(url)}
+				</a>
+			) : null}
+		</li>
 	);
 }
