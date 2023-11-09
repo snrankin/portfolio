f264adb8532bf43f22e471da697f2ae0f96e17c2
Sam Rankin <samrankin.dev@gmail.com>
2023-11-08 21:58:23 -0700

    refactor(about.tsx): update import paths for Section and image assets to use absolute paths for better maintainability and readability feat(about.tsx): add ThemeSwap component to toggle between dark and light frames for better user experience

--- src/app/(pages)/(home)/about.tsx
+++ src/app/(pages)/(home)/about.tsx
@@ -1,9 +1,11 @@
 'use client';
 import React, { useState } from 'react';
 import Image from 'next/image';
-import Section from '../../components/section/section';
+import Section from '@/app/components/section/section';
+import ThemeSwap from '@/app/components/theme-swap/theme-swap';
 import MeImg from '@/img/Me.png';
-import ImgFrame from '@/img/frame.svg';
+import DarkFrame from '@/img/frame-dark.svg';
+import LightFrame from '@/img/frame-light.svg';
 import { Me } from '@/app/lib/me';
 export default function About(): JSX.Element {
 	return (
@@ -22,10 +24,18 @@ export default function About(): JSX.Element {
 							className="w-full h-auto mask mask-diamond grayscale will-change-auto hover:grayscale-0"
 							loading="lazy"
 							fill={true}
+							sizes="100vw"
 						/>
 					</div>
 					<div className="absolute w-full h-full inset-0 flex">
-						<ImgFrame className="block w-full h-full grow stroke-[20px] md:stroke-[15px]" />
+						<ThemeSwap
+							light={
+								<DarkFrame className="block w-full h-full grow stroke-[20px] md:stroke-[15px]" />
+							}
+							dark={
+								<LightFrame className="block w-full h-full grow stroke-[20px] md:stroke-[15px]" />
+							}
+						/>
 					</div>
 				</div>
 				<div className="prose lg:prose-lg max-w-none">
