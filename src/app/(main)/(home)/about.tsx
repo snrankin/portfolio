1755d8f29221a8f551290c01c5d9693503aaac41
Sam Rankin <samrankin.dev@gmail.com>
2023-11-17 16:21:12 -0700

    refactor(about.tsx): improve code readability and maintainability by using destructuring assignment for props object feat(about.tsx): update Section component props to use values from props object for better flexibility and reusability feat(about.tsx): update ContentfulImage component props to use values from props object for better flexibility and reusability feat(about.tsx): update Markdown component props to use values from props object for better flexibility and reusability

--- src/app/(main)/(home)/about.tsx
+++ src/app/(main)/(home)/about.tsx
@@ -7,33 +7,20 @@ import ThemeSwap from '@/components/theme-swap/theme-swap';
 import DarkFrame from '@/img/frame-dark.svg';
 import LightFrame from '@/img/frame-light.svg';
 import { IAuthor } from '@/lib/api/authors';
-export default function About({ me }: { me?: IAuthor }): JSX.Element {
+import { TypeAboutSectionFields } from '@/lib/types';
+export default function About(props?: TypeAboutSectionFields): JSX.Element {
 	return (
 		<Section
-			id="about"
-			title="About Me"
-			command="$(whoami)"
-			flags="verbose"
+			id={`${props?.slug}`}
+			title={`${props?.title}`}
+			command={`${props?.command}`}
+			argument={`${props?.argument}`}
+			flags={props?.flags}
+			intro={props?.intro}
 		>
 			<div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] items-center gap-11">
-				{!!me?.picture && (
+				{!!props?.image && (
 					<div className="relative">
-						<div className="aspect-w-1 aspect-h-1">
-							<ContentfulImage
-								src={me.picture?.url}
-								className="w-full h-auto mask mask-diamond grayscale will-change-auto hover:grayscale-0"
-								loading="lazy"
-								fill={true}
-								sizes="100vw"
-								// style={{
-								// 	width: '100%',
-								// 	height: 'auto',
-								// }}
-								alt={me.picture.description}
-								// width={me.picture.width}
-								// height={me.picture.height}
-							/>
-						</div>
 						<div className="absolute w-full h-full inset-0 flex">
 							<ThemeSwap
 								light={
@@ -44,11 +31,27 @@ export default function About({ me }: { me?: IAuthor }): JSX.Element {
 								}
 							/>
 						</div>
+						<div className="aspect-w-1 aspect-h-1">
+							<ContentfulImage
+								src={props.image.url}
+								className="w-full h-auto mask ease-in-out duration-300 grayscale will-change-auto hover:grayscale-0"
+								loading="lazy"
+								fill={true}
+								sizes="100vw"
+								// style={{
+								// 	width: '100%',
+								// 	height: 'auto',
+								// }}
+								alt={`${props.image.description}`}
+								// width={me.picture.width}
+								// height={me.picture.height}
+							/>
+						</div>
 					</div>
 				)}
-				{!!me?.about && (
+				{!!props?.content && (
 					<div className="prose lg:prose-lg max-w-none">
-						<Markdown content={me.about} />
+						<Markdown content={props.content} />
 					</div>
 				)}
 			</div>
