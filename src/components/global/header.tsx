eef74fac103bbc56a71b6efb3780b5e099531857
Sam Rankin <samrankin.dev@gmail.com>
2023-11-27 15:25:33 -0700

    fix: fixing cta responsive and header safe padding

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -29,39 +29,47 @@ export default function Header(props: HeaderProps) {
 	};
 
 	return (
-		<div className="relative navbar px-pg lg:px-4">
-			{/* Hamburger button for mobile view */}
+		<div className="pt-envt">
+			<div className="relative navbar px-pg lg:px-4">
+				{/* Hamburger button for mobile view */}
 
-			<div className="navbar-start">
-				<Link
-					href="/"
-					className="w-[40px] h-[40px] lg:w-[60px] lg:h-[60px] block"
-				>
-					<Logo className="w-[40px] h-[40px] lg:w-[60px] lg:h-[60px]" />
-				</Link>
-			</div>
-			<div className="navbar-center" ref={ref} onClick={closeDropdown}>
-				<nav
-					{...getCollapseProps()}
-					className=" absolute top-full right-0 md:relative md:top-auto md:right-auto w-screen md:w-auto md:!flex md:!h-auto md:!overflow-visible grow dark:bg-slate-900 bg-base-100 md:!bg-transparent shadow md:shadow-none"
-				>
-					<NavBar links={props.links} />
-				</nav>
-			</div>
-			<div className="navbar-end">
-				<ThemeButton />
-				<button
-					{...getToggleProps({ onClick: toggleDropdown })}
-					className="md:hidden flex flex-col justify-center border-0 bg-transparent px-2 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 lg:hidden"
-				>
-					<span
-						className={`${styles.hamburger} ${styles.hamburgerSpin}`}
+				<div className="navbar-start">
+					<Link
+						href="/"
+						className="w-[40px] h-[40px] lg:w-[60px] lg:h-[60px] block"
 					>
-						<span className={`${styles.hamburgerBox}`}>
-							<span className={`${styles.hamburgerInner}`}></span>
+						<Logo className="w-[40px] h-[40px] lg:w-[60px] lg:h-[60px]" />
+					</Link>
+				</div>
+				<div
+					className="navbar-center"
+					ref={ref}
+					onClick={closeDropdown}
+				>
+					<nav
+						{...getCollapseProps()}
+						className=" absolute top-full right-0 md:relative md:top-auto md:right-auto w-screen md:w-auto md:!flex md:!h-auto md:!overflow-visible grow dark:bg-slate-900 bg-base-100 md:!bg-transparent shadow md:shadow-none"
+					>
+						<NavBar links={props.links} />
+					</nav>
+				</div>
+				<div className="navbar-end">
+					<ThemeButton />
+					<button
+						{...getToggleProps({ onClick: toggleDropdown })}
+						className="md:hidden flex flex-col justify-center border-0 bg-transparent px-2 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 lg:hidden"
+					>
+						<span
+							className={`${styles.hamburger} ${styles.hamburgerSpin}`}
+						>
+							<span className={`${styles.hamburgerBox}`}>
+								<span
+									className={`${styles.hamburgerInner}`}
+								></span>
+							</span>
 						</span>
-					</span>
-				</button>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
