aeaf8f7993bdf4d4e067ba511a1f6cd957dc9db0
Sam Rankin <samrankin.dev@gmail.com>
2023-12-02 05:00:37 -0700

    feat: styling error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,38 @@
+import Link from 'next/link';
+
+import Icon from '@/components/icons/icon-item';
+import Section from '@/components/section/section';
+import styles from '@/css/error.module.css';
+import ThemeProvider from '@/lib/context/theme';
+import { APOS } from '@/lib/symbols';
+
 export default function NotFound() {
 	return (
-		<div className="grid h-screen px-4 bg-base-100 place-content-center">
-			<div className="text-center">
-				<h1 className="font-black text-9xl">404</h1>
+		<ThemeProvider>
+			<Section
+				id="error-404"
+				className={`${styles.errorBg} bg-base-200 min-h-screen dark:bg-slate-900 flex flex-col justify-center items-center`}
+				containerClasses="pt-[25vw] sm:pb-[10vw] md:pt-[20vw] xl:pt-[15vw] xl:pb-[5vw] 2xl:pt-64 2xl:pb-20 relative z-10"
+			>
+				<div className="prose prose-2xl max-w-[96%] md:max-w-[70%] lg:prose-lg prose-h1:text-14xl sm:prose-h1:text-6xl md:prose-h1:text-4xl xl:prose-h1:text-8xl text-center mx-auto">
+					<h1 className={`!mb-0 ${styles.title}`}>
+						404{' '}
+						<small className="block font-semibold text-[0.5em] leading-none">
+							Uh-oh!
+						</small>
+					</h1>
 
-				<p className="text-2xl font-bold tracking-tight text-base-900 sm:text-4xl">
-					Uh-oh!
-				</p>
+					<p className="mt-4">We can{APOS}t find that page.</p>
 
-				<p className="mt-4 text-base-500">
-					We can`&apos;`t find that page.
-				</p>
-
-				<a href="/" className="btn btn-primary">
-					Go Back Home
-				</a>
-			</div>
-		</div>
+					<Link href="/" className="btn btn-lg btn-primary">
+						<Icon
+							icon="home"
+							title="Go Back Home"
+							titleDisplay="inline"
+						/>
+					</Link>
+				</div>
+			</Section>
+		</ThemeProvider>
 	);
 }
